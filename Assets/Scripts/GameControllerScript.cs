c87cf7eecf52bac11293684320d87a06481cb4d6
McGibbo <robin.zeijlon@telia.com>
2017-01-24 16:52:48 +0100

    Camera
    Fixade så att kameran följer spelarens x-axel och lade in camerashake som ökar ju högre spelarens fart är vid kollisionen.

---
+++ Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameControllerScript : MonoBehaviour
+{
+
+    [SerializeField]float shakeAmount;
+    GameObject player;
+    Camera mainCamera;
+    CameraScript cameraScript;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        mainCamera = Camera.main;
+        cameraScript = mainCamera.GetComponent<CameraScript>();
+    }
+
+
+    void Update()
+    {
+        MoveCamera();
+    }
+
+    void MoveCamera()
+    {
+        if (player.transform.position.x > mainCamera.transform.position.x)
+        {
+            mainCamera.transform.position = new Vector3(player.transform.position.x, mainCamera.transform.position.y, mainCamera.transform.position.z);
+        }
+    }
+
+    public void ScreenShake(float playerYSpeed)
+    {
+        Debug.Log("GameController");
+        cameraScript.StartScreenShake(playerYSpeed * 100000000000 * shakeAmount);
+        Debug.Log(playerYSpeed * 100000000000 * shakeAmount);
+    }
+
+}
---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+    private Vector3 playerPosition;
+    public float speed = 5f;
+    public float jumpForce = 100f;
+
+    private bool standingOnGround = true;
+    private bool hasChangedGravity = false;
+
+    public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerJump();
+        playerConstantRunning();
+        playerChangeGravity();
+	}
+
+    void playerConstantRunning()
+    {
+        //Vector3 position = transform.position;
+        //position[0] += speed;
+        //transform.position = position;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(10, GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    /*
+    void OnCollisionStay(Collision)
+    {
+
+    }
+    */
+
+    void playerJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (standingOnGround == true)
+            {
+                if (hasChangedGravity == false)
+                {
+                    GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+                }
+                else
+                {
+                    GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, -jumpForce));
+                }
+            }
+        }
+    }
+    
+    void playerChangeGravity()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Physics2D.gravity *= -1;
+            hasChangedGravity = !hasChangedGravity;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.gameObject.tag == "Platform")
+        {
+            SendMessageUpwards("ScreenShake", GetComponent<Rigidbody2D>().velocity.y);
+        }
+    }
+}

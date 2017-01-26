675ded71f4b801544cf164ab67fdba84befcb61b
PoroSenpai <basket-boll@live.se>
2017-01-26 14:51:08 +0100

    Add files via upload

---
+++ Upload/DrawLevel.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class ColorToPrefab
+{
+    public Color32 color;
+    public GameObject prefab;
+}
+
+public class DrawLevel : MonoBehaviour
+{
+    public Texture2D levelMap;
+    public ColorToPrefab[] colorToPrefab;
+
+	void Start () {
+        LoadMap();        
+	}
+
+    void LoadMap()
+    {
+        Color32[] allPixels = levelMap.GetPixels32();
+        int width = levelMap.width;
+        int height = levelMap.height;
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                SpawnTile(allPixels[(y * width) + x], x, y);
+            }
+        }
+    }
+
+    void SpawnTile(Color32 c, int x, int y)
+    {
+        if (c.a <= 0)
+            return;
+
+        foreach (ColorToPrefab ctp in colorToPrefab)
+        {
+            if (c.Equals(ctp.color))
+            {
+                Instantiate(ctp.prefab, new Vector3(x, y, 0), Quaternion.identity);
+                return;
+            }
+        }
+        Debug.LogError("Incorrect Color: " + c.ToString() + "\n Position: (" + x.ToString() + "x, " + y.ToString() + "y) from the bottom left of the screen");
+    }
+}
---
+++ Upload/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D player;
+
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            player.velocity = new Vector2(-5, player.velocity.y);
+        if (Input.GetKeyDown(KeyCode.D))
+            player.velocity = new Vector2(5, player.velocity.y);
+        if (Input.GetKey(KeyCode.Space))
+            player.velocity = new Vector2(player.velocity.x, 5);
+    }
+}

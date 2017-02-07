196c7cc9bd816802885bd7a25309b756af787956
PoroSenpai <basket-boll@live.se>
2017-02-07 12:13:09 +0100

    Add files via upload

---
+++ Kalyrim/Assets/Scripts/DrawCollisionBox.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawCollisionBox : MonoBehaviour
+{
+    public GameObject CollisionBox;
+    float leftValue;
+    float rightValue;
+    //DrawLevel draw;
+
+    void Start()
+    {
+        //draw = GetComponent<DrawLevel>();
+        //LoadMap();
+    }
+
+    //void LoadMap()
+    //{
+
+    //    Color32[] allPixels = draw.levelMap.GetPixels32();
+    //    int width = draw.levelMap.width;
+    //    int height = draw.levelMap.height;
+
+    //    for (int y = 0; y < height; y++)
+    //    {
+    //        for (int x = 0; x < width; x++)
+    //        {
+    //            SpawnTile(allPixels[(y * width) + x], x, y);
+    //        }
+    //    }
+    //}
+
+    public void SaveLeftValue(int x)
+    {
+        leftValue = x;
+    }
+
+    public void SaveRightValue(int x)
+    {
+        rightValue = x;
+    }
+
+    public void SpawnCollisionPlatform(int y)
+    {
+        CollisionBox.transform.localScale = new Vector3(rightValue - leftValue + 1, 1, 1);
+        Instantiate(CollisionBox, new Vector2(leftValue + (rightValue - leftValue) / 2, y), Quaternion.identity);
+        Debug.Log("Spawned at" + new Vector2(leftValue + (rightValue - leftValue) / 2, y).ToString() + "y = " + y.ToString());
+    }
+
+
+    //void SpawnTile(Color32 c, int x, int y)
+    //{
+    //    if (c.a <= 0)
+    //        return;
+
+    //    foreach (ColorToPrefab ctp in draw.colorToPrefab)
+    //    {
+    //        switch (ctp.objectTag)
+    //        {
+    //            case ColorToPrefab.ObjectTag.None:
+    //                return;
+    //                break;
+
+    //            case ColorToPrefab.ObjectTag.LeftPlatform:
+    //                SaveLeftValue(x);
+    //                break;
+
+    //            case ColorToPrefab.ObjectTag.RightPlatform:
+    //                SaveRightValue(x);
+    //                SpawnCollisionPlatform(y);
+                   
+    //                break;
+    //        }
+    //    }
+    //}
+}
--- Kalyrim/Assets/Scripts/DrawLevel.cs
+++ Kalyrim/Assets/Scripts/DrawLevel.cs
@@ -6,38 +6,47 @@ public class ColorToPrefab
 {
     public Color32 color;
     public GameObject prefab;
+    public ObjectTag objectTag;
+    public enum ObjectTag {
+        None,
+        LeftPlatform,
+        RightPlatform
+    }
 }
 
 public class DrawLevel : MonoBehaviour
 {
     public Texture2D levelMap;
     public ColorToPrefab[] colorToPrefab;
+    DrawCollisionBox dcb;
 
-	void Start () {
+	void Start ()
+    {
+        dcb = GetComponent<DrawCollisionBox>();
         LoadMap();
 	}
 
-    void EmptyMap()
-    {
-        while(transform.childCount > 0)
-        {
-            Transform c = transform.GetChild(0);
-            c.SetParent(null);
-            Destroy(c.gameObject);
-        }
-    }
+    //void EmptyMap()
+    //{
+    //    while(transform.childCount > 0)
+    //    {
+    //        Transform c = transform.GetChild(0);
+    //        c.SetParent(null);
+    //        Destroy(c.gameObject);
+    //    }
+    //}
 
     void LoadMap()
     {
-        EmptyMap();
+        //EmptyMap();
 
         Color32[] allPixels = levelMap.GetPixels32();
         int width = levelMap.width;
         int height = levelMap.height;
 
-        for (int x = 0; x < width; x++)
+        for (int y = 0; y < height; y++)
         {
-            for (int y = 0; y < height; y++)
+            for (int x = 0; x < width; x++)
             {
                 SpawnTile(allPixels[(y * width) + x], x, y);
             }
@@ -55,8 +64,26 @@ public class DrawLevel : MonoBehaviour
             if (c.Equals(ctp.color))
             {
                 Instantiate(ctp.prefab, new Vector3(x, y, 0), Quaternion.identity);
+
+
+
+                switch (ctp.objectTag)
+                {
+                    case ColorToPrefab.ObjectTag.None:
+                        return;
+
+                    case ColorToPrefab.ObjectTag.LeftPlatform:
+                        dcb.SaveLeftValue(x);
+                        break;
+
+                    case ColorToPrefab.ObjectTag.RightPlatform:
+                        dcb.SaveRightValue(x);
+                        dcb.SpawnCollisionPlatform(y);
+                        break;
+                }
                 return;
             }
+            
         }
         Debug.LogError("Incorrect Color: " + c.ToString() + "\n Position: (" + x.ToString() + "x, " + y.ToString() + "y) from the bottom left of the screen");
         
